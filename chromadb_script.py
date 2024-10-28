231e8dc0cad6ff5fb45713d4327dafe99bc641cd
Noufu <nouifer445@gmail.com>
2024-10-28 17:55:38 +0300

    added retriever componenet

--- chromadb_script.py
+++ chromadb_script.py
@@ -1,5 +1,4 @@
 from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_community.vectorstores import Chroma
 import os
---
+++ retriever.py
@@ -0,0 +1,12 @@
+from langchain_community.vectorstores import Chroma
+from chromadb_script import (CHROMA_PATH, embeddings)
+from langchain.schema import Document
+
+class Retriever():
+    def __init__(self):
+       self.vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=embeddings)
+
+    def get_context(self, query:str) -> list[Document]:
+        retriever = self.vectordb.as_retriever(search_kwargs={'k':2})
+        docs = retriever.get_relevant_documents(query)
+        return docs
