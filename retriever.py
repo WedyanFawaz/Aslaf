a9580e77c4d77ec4e6e2df1b58fd08c8edc19abe
fattalism <noufier445@gmail.com>
2024-11-09 01:36:20 -0500

    deleted unnessary comments

--- retriever.py
+++ retriever.py
@@ -18,10 +18,6 @@ class Retriever():
         # Perform similarity search with relevance scores
         docs_with_scores = self.vectordb.similarity_search_with_relevance_scores(query, k=3)
 
-        # Filter documents with a score of at least 0.71
-        # filtered_docs = [
-        #     doc for doc, score in docs_with_scores if score >= 0.71
-        # ]
 
         # Generate context and filtered sources
         context, resources = self.format_docs(docs_with_scores)
